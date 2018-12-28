ccba082952bce48beb28fa0bf2a2f896de5b3401
Cristian Batista <cristian.batista@edosoft.es>
2018-12-28 10:34:27 +0000

    STARFISH1-1 Create architecture
    > Created project with Spring initialzr
    > Added LICENSE project

---
+++ src/main/java/com/starfish/starfish/StarfishApplication.java
@@ -0,0 +1,13 @@
+package com.starfish.starfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarfishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarfishApplication.class, args);
+	}
+
+}
